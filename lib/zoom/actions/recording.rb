56aa289c6464d45b79398fcb3efd3ebd7379e2be
David Lesches <david@lesches.com>
2020-07-01 16:40:14 -0400

    Verzion

--- lib/zoom/actions/recording.rb
+++ lib/zoom/actions/recording.rb
@@ -6,13 +6,13 @@ module Zoom
       def recording_list(*args)
         options = Zoom::Params.new(Utils.extract_options!(args))
         options.require(%i[user_id])
-        Utils.parse_response(self.class.get("/users/#{options[:user_id]}/recordings", query: options.except(:user_id), headers: request_headers)
+        Utils.parse_response self.class.get("/users/#{options[:user_id]}/recordings", query: options.except(:user_id), headers: request_headers)
       end
 
       def recording_get(*args)
         options = Zoom::Params.new(Utils.extract_options!(args))
         options.require(%i[meeting_id])
-        Utils.parse_response(self.class.get("/meetings/#{options[:meeting_id]}/recordings", query: options.except(:meeting_id), headers: request_headers)
+        Utils.parse_response self.class.get("/meetings/#{options[:meeting_id]}/recordings", query: options.except(:meeting_id), headers: request_headers)
       end
     end
   end
